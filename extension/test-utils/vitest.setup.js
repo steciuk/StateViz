87f4d5dc0153adb3992e67d2d1cea3e307d1948d
Adam Steciuk <a.t.steciuk@gmail.com>
2023-12-09 19:10:23 +0100

    Install vitest-chrome

--- extension/test-utils/vitest.setup.js
+++ extension/test-utils/vitest.setup.js
@@ -1,2 +1,4 @@
-// Do what you need to set up your test
-console.log('setup test: vitest.setup.js');
+// for some reason vitest is using CommonJS, so we need to import it like this
+import * as chrome from 'vitest-chrome/lib/index.esm.js';
+
+Object.assign(global, chrome);
