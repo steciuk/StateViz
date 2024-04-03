92e030a4291bc8f4b6ef96e6c623b5b86c1fec0f
steciuk <a.t.steciuk@gmail.com>
2024-04-03 18:29:17 +0200

    Add IfElse and Slot to benchmark demo

---
+++ demo/benchmark/src/svelte/Slot.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<div>
+  <h2>{name}</h2>
+  <div>
+    <slot />
+  </div>
+</div>
--- demo/benchmark/src/svelte/SvelteApp.svelte
+++ demo/benchmark/src/svelte/SvelteApp.svelte
@@ -1,10 +1,16 @@
 <script>
   import Component from "./Component.svelte";
   import Eaches from "./Eaches.svelte";
+  import IfElse from "./IfElse.svelte";
+  import Slot from "./Slot.svelte";
 </script>
 
 <div>
   <h1>Svelte</h1>
   <Component name="Component" />
   <Eaches name="Eaches" />
+  <Slot name="Slot">
+    <Component name="Component in Slot" />
+  </Slot>
+  <IfElse name="IfElse" />
 </div>
